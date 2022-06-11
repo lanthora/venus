174cb2f13a67dfeed0a3b8e244d4256ed172faf2
LANTHORA <lanthora@outlook.com>
2022-06-11 20:15:53 +0800

    使用循环处理组件

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
-import TabPanel from "./components/TabPanel"
-import BaseInfo from "./components/BaseInfo"
-import Process from "./components/Process"
-import File from "./components/File"
-import Net from "./components/Net"
-import Extension from "./components/Extension"
+import TabPanel from './components/TabPanel'
+import BaseInfo from './components/BaseInfo'
+import Process from './components/Process'
+import File from './components/File'
+import Net from './components/Net'
+import Extension from './components/Extension'
 
 
 function fillTabProps(group, index) {
@@ -17,40 +17,35 @@ function fillTabProps(group, index) {
   };
 }
 
-export default function App() {
+function App() {
   const [value, setValue] = React.useState(0);
-  const group = "sample";
+  const group = "uranus";
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
+  const tabs = [
+    { label: "概览", page: <BaseInfo /> },
+    { label: "进程防护", page: <Process /> },
+    { label: "文件防护", page: <File /> },
+    { label: "网络防护", page: <Net /> },
+    { label: "扩展功能", page: <Extension /> },
+  ]
+
   return (
     <Box sx={{ width: '100%' }}>
+
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab label="概览" {...fillTabProps(group, 0)} />
-          <Tab label="进程防护" {...fillTabProps(group, 1)} />
-          <Tab label="文件防护" {...fillTabProps(group, 2)} />
-          <Tab label="网络防护" {...fillTabProps(group, 3)} />
-          <Tab label="扩展功能" {...fillTabProps(group, 4)} />
+          {tabs.map((item, idx) => (<Tab label={item.label} {...fillTabProps(group, idx)} />))}
         </Tabs>
       </Box>
-      <TabPanel group={group} value={value} index={0}>
-        <BaseInfo />
-      </TabPanel>
-      <TabPanel group={group} value={value} index={1}>
-        <Process />
-      </TabPanel>
-      <TabPanel group={group} value={value} index={2}>
-        <File />
-      </TabPanel>
-      <TabPanel group={group} value={value} index={3}>
-        <Net />
-      </TabPanel>
-      <TabPanel group={group} value={value} index={4}>
-        <Extension />
-      </TabPanel>
+
+      {tabs.map((item, idx) => (<TabPanel group={group} value={value} index={idx}> {item.page} </TabPanel>))}
+
     </Box>
   );
 }
+
+export default App;
