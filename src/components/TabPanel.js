25a0bcbc71b5ef6326e1176f9dfea068f2b388cd
lanthora <lanthora@outlook.com>
2022-06-11 12:12:56 +0000

    使用循环处理组件

--- src/components/TabPanel.js
+++ src/components/TabPanel.js
@@ -4,30 +4,30 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
 function TabPanel(props) {
-    const { children, group, value, index, ...other } = props;
+  const { children, group, value, index, ...other } = props;
 
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`${group}-tabpanel-${index}`}
-            aria-labelledby={`${group}-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box sx={{ p: 3 }}>
-                    <Typography>{children}</Typography>
-                </Box>
-            )}
-        </div>
-    );
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`${group}-tabpanel-${index}`}
+      aria-labelledby={`${group}-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
 }
 
 TabPanel.propTypes = {
-    children: PropTypes.node,
-    group: PropTypes.string.isRequired,
-    index: PropTypes.number.isRequired,
-    value: PropTypes.number.isRequired,
+  children: PropTypes.node,
+  group: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
 };
 
 export default TabPanel;
