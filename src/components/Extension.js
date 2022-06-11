46112d42758287b0e958e66f9264a21d74f2213c
LANTHORA <lanthora@outlook.com>
2022-06-11 19:14:47 +0800

    拆分功能组件

---
+++ src/components/Extension.js
@@ -0,0 +1,12 @@
+function Extension(props) {
+  return (
+    <pre> {`
+      Extension.js
+
+      操作:
+        1. 配置 Telegram Bot 机器人,上报日志.列表显示已经配置的 Bot 以及运行状态
+      `} </pre>
+  )
+}
+
+export default Extension;
