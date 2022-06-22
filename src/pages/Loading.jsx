50be155c0accd4a740038408e265684ac5049d2f
lanthora <lanthora@outlook.com>
2022-06-22 15:12:36 +0000

    优化登录页和加载页提示

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import axios from "../components/axios"
 import history from '../components/history'
+import Box from '@mui/material/Box'
+import CircularProgress from '@mui/material/CircularProgress'
+
 
 function Loading(props) {
   React.useEffect(() => {
@@ -11,12 +14,19 @@ function Loading(props) {
     })
   })
 
-  return (
-    <pre> {`
-      Loading.js
+  const center = {
+    position: "absolute",
+    left: "50%",
+    top: "50%",
+    transform: "translate(-50%, -50%)",
+  }
 
-      加载中
-      `} </pre>
+  return (
+    <div style={center}>
+      <Box sx={{ display: 'flex' }}>
+        <CircularProgress />
+      </Box>
+    </div>
   )
 }
 
