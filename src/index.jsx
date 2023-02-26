8d8ffc3742b6ead66db87f9133439ed99817b18a
lanthora <lanthora@outlook.com>
2023-02-26 10:48:41 +0000

    添加监控大屏页面

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from 'pages/App'
 import Login from 'pages/Login'
+import Monitor from 'pages/Monitor'
 import history from 'components/history'
 import HistoryRouter from 'components/HistoryRouter'
 import { Route, Routes } from 'react-router-dom'
@@ -20,6 +21,7 @@ root.render(
       <Routes>
         <Route path='/login' element={<Login />} />
         <Route path='/app' element={<App />} />
+        <Route path='/monitor' element={<Monitor />} />
         <Route path='*' element={<Loading />} />
       </Routes>
     </HistoryRouter>
---
+++ src/pages/Monitor.jsx
@@ -0,0 +1,10 @@
+import { Box } from '@mui/material';
+import React from 'react'
+
+export default function App() {
+  return (
+    <Box>
+      监控大屏
+    </Box>
+  )
+}
